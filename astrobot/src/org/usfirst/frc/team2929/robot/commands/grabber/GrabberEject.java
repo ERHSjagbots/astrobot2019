a32c35c0d7c73fbf13be753a008dd84f8579c472
JagBots3 <JagBots3@10.52.100.130>
2019-01-28 19:23:32 -0800

    Catching up to Master branch

---
+++ astrobot/src/org/usfirst/frc/team2929/robot/commands/grabber/GrabberEject.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team2929.robot.commands.grabber;
+
+import org.usfirst.frc.team2929.robot.commands.Timeout;
+import org.usfirst.frc.team2929.robot.utility.PistonSelect;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class GrabberEject extends CommandGroup {
+	
+	public GrabberEject() {
+		addSequential(new GrabberPush(PistonSelect.FORWARD));
+		addSequential(new Timeout(0.5));
+		addSequential(new GrabberPush(PistonSelect.REVERSE));
+		addSequential(new GrabberPush(PistonSelect.OFF));
+	}
+	
+}
